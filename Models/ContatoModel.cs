058ebb5bce359039c09378d43bd1deea58cb346e
SrFerreiraDev <srferreiradev@gmail.com>
2022-04-19 13:25:12 -0300

    Inicio do Projeto

---
+++ Models/ContatoModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o Nome do Contato!")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o E-mail do Contato!")]
+        [EmailAddress(ErrorMessage = "O e-mail informado não é válido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Digite o número de Celular do Contato!")]
+        [Phone(ErrorMessage = "O número de Celular informado não é válido!")]
+        public string Celular { get; set; }
+    }
+}
